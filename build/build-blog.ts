77b63da6b7bdf3505e2fb95082a8c6f86a956a05
SAN.D <31897843+SANKARAMDAS@users.noreply.github.com>
2023-07-03 19:10:42 +0530

    Update build-blog.ts

--- build/build-blog.ts
+++ build/build-blog.ts
@@ -3,3 +3,4 @@ import { buildBlogFeed, buildBlogIndex, buildBlogPosts } from "./blog.js";
 await buildBlogIndex({ verbose: true });
 await buildBlogPosts({ verbose: true });
 await buildBlogFeed({ verbose: true });
+
